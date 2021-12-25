c8be0f5e72ba62f3668e5de5b83052c44ec9181c
João Vítor <joaovitorpessoa10@gmail.com>
2021-12-25 13:31:15 -0300

    Atualização 1.1
    Adição do pdf sobre listas e mais exercícios.

--- exercicios/ex016.py
+++ exercicios/ex016.py
@@ -1,1 +1,3 @@
-aaa
+#Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção Inteira.
+n = float(input("Digite o número: "))
+print("A parte inteira do número corresponde a {}".format(int(n)))
---
+++ exercicios/ex018.py
@@ -0,0 +1,3 @@
+lista = ["banana","Laranja","Kiwi","cereja"]
+lista.sort(key = str.lower)
+print(lista)
