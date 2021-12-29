f81b46c2f55e7154fe33830a1b230e8b40b489a6
João Vítor <joaovitorpessoa10@gmail.com>
2021-12-29 14:40:40 -0300

    Atualização 1.4 Baixando o Pycharm e utilizando para rodar o módulo pygame

--- exercicios/ex021.py
+++ exercicios/ex021.py
@@ -1,5 +1,6 @@
-#Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
+# Faça um programa em Python que abra e reproduza o áudio de um arquivo MP3.
 import pygame
+
 pygame.mixer.init()
 pygame.init()
 pygame.mixer.music.load('ex021_Tokyo.mp3')
