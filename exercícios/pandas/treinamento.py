21506584ee32fe646c4e6baafbc93d8bbc85ed36
João Vítor <89874527+lLittleJohnl@users.noreply.github.com>
2022-02-21 16:28:01 -0300

    Escrevendo em HTML
    Cogitando escrever em Markdown e HTML.

--- exercícios/pandas/treinamento.py
+++ exercícios/pandas/treinamento.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('data.csv','r')
+df = pd.read_csv('C:\\Users\\joaov\\OneDrive\\Documentos\\Github\\Python\\exercícios\\pandas\\data.csv')
 df.plot()
 plt.show()
