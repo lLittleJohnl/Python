0c9aaa3c961a6a41a600f6fbc8b0a3d67b0e6103
João Vítor <89874527+lLittleJohnl@users.noreply.github.com>
2022-03-01 10:49:08 -0300

    Update
    Melhorando os textos

---
+++ exercicios/pandas/cisco.py
@@ -0,0 +1,5 @@
+dct = {'one':'two', 'three':'one', 'two':'three'}
+v = dct["three"]
+for k in range(len(dct)):
+    v = dct[v]
+print(v)
--- exercicios/pandas/treinamento.py
+++ exercicios/pandas/treinamento.py
@@ -1,6 +1,8 @@
-import pandas as pd
-import matplotlib.pyplot as plt
+import pandas
 
-df = pd.read_csv('C:\\Users\\joaov\\OneDrive\\Documentos\\Github\\Python\\exercícios\\pandas\\data.csv')
-df.plot()
-plt.show()
+mydataset = {
+            'cars': ["BMW", "Volvo", "Ford"],
+            'passings': [3, 7, 2]
+            }
+myvar = pandas.DataFrame(mydataset)
+print(myvar)
